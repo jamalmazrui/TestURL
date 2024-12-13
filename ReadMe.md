93f2b149eceeaaffe4ea463849f941bf4b259419
Jamal Mazrui <jamal@empowermentzone.com>
2024-12-12 19:26:15 -0800

    Improve docs, add batch file, and make JavaScript prettier.

--- ReadMe.md
+++ ReadMe.md
@@ -24,11 +24,13 @@ Run the program: \
 
 where \<URL\> is the web address of the page to be tested for conformance to the [Web Content Accessibility Guidelines](https://www.w3.org/TR/WCAG22/) (WCAG).
 
-Rather than passing a URL as a parameter, a file name may be passed instead. Such a text file lists multiple URLs, one per line. Use the syntax:
+Rather than passing a URL as a parameter, a file name may be passed instead. Such a text file lists multiple URLs, one per line: \
+`node testUrl.js <File>`
 
-`TestURL.cmd <File>`
+On Windows, a batch file, TestURL.cmd, is also available: \
+`TestURL.cmd <URL or File>`
 
-The program will run the Microsoft Edge browser with the API of [Deque Axe](https://www.npmjs.com/package/axe-core). The resulting HTML report will have a unique file name in the current directory, based on the title of the web page tested.
+The program will run the Microsoft Edge browser with the API of [Deque Axe](https://www.npmjs.com/package/axe-core). The resulting HTML report will have a unique file name in the current directory, based on the title of the web page tested, and if needed, a numeric suffix.
 
 If a URL was passed as a parameter, the HTML report will be opened when the program ends. If a file was passed instead, a report is not opened automatically.
 
--- testUrl.js
+++ testUrl.js
@@ -1,9 +1,9 @@
-const axeCore = require('axe-core');
+const axeCore = require("axe-core");
 const fs = require("fs");
 const opener = require("opener");
 const path = require("path");
 const process = require("process");
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const { reporterFactory } = require("accessibility-insights-report");
 
 function getUniqueFile(sDir, sRoot, sExt) {
@@ -21,61 +21,64 @@ function getUniqueFile(sDir, sRoot, sExt) {
 
 async function main(sUrl) {
   let aLines = await [sUrl];
-let bUrl = true;
+  let bUrl = true;
   if (await !sUrl.includes("://")) {
-bUrl = false;
+    bUrl = false;
     let sLines = await fs.readFileSync(sUrl, "utf-8");
     aLines = await sLines.split("\n");
   }
 
-const sCurDir = await process.cwd();
-let sOutputHtml;
-const browser = await puppeteer.launch({
-executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
-headless: true
-});
+  const sCurDir = await process.cwd();
+  let sOutputHtml;
+  const browser = await puppeteer.launch({
+    executablePath:
+      "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+    headless: true,
+  });
 
   while (aLines.length > 0) {
     sUrl = await aLines.shift();
     sUrl = await sUrl.trim();
     if (await !sUrl) continue;
 
-console.log();
-console.log("Testing " + sUrl);
-const page = await browser.newPage();
-await page.goto(sUrl);
+    console.log();
+    console.log("Testing " + sUrl);
+    const page = await browser.newPage();
+    await page.goto(sUrl);
 
     let sPageTitle = await page.title();
     sPageTitle = await sPageTitle.replace(/[^a-zA-Z0-9_,#\.\-]/gi, " ");
     sPageTitle = await sPageTitle.replace(/\s+/gi, " ").trim();
-console.log(sPageTitle);
+    console.log(sPageTitle);
 
     await page.evaluate(axeCore.source);
-const results = await page.evaluate(() => {
+    const results = await page.evaluate(() => {
       return axe.run();
     });
 
-   const reporter = reporterFactory();
-    const sHtml = reporter.fromAxeResult({
+    const reporter = reporterFactory();
+    const sHtml = reporter
+      .fromAxeResult({
         results,
         description: "description of report",
         serviceName: "service name",
         scanContext: {
-pageTitle: sPageTitle,
-        }
-    }).asHTML();
+          pageTitle: sPageTitle,
+        },
+      })
+      .asHTML();
 
-sOutputHtml = await getUniqueFile(sCurDir, sPageTitle, "htm");
-await fs.writeFileSync(sOutputHtml, sHtml);
-await page.close();
-} // while
-await browser.close();
-if (bUrl) await opener(sOutputHtml);
+    sOutputHtml = await getUniqueFile(sCurDir, sPageTitle, "htm");
+    await fs.writeFileSync(sOutputHtml, sHtml);
+    await page.close();
+  } // while
+  await browser.close();
+  if (bUrl) await opener(sOutputHtml);
 }
 
 const sUrl = process.argv[2];
 if (!sUrl) {
-console.error('Please provide a URL or file as a command line argument');
-process.exit(1);
+  console.error("Please provide a URL or file as a command line argument");
+  process.exit(1);
 }
 main(sUrl);
