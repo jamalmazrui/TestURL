e4801486a119ee4cb5d1b80590b28ed5fbd6a737
Jamal Mazrui <jamal@empowermentzone.com>
2024-12-12 17:49:04 -0800

    Initial commit.

---
+++ ReadMe.md
@@ -0,0 +1,35 @@
+﻿# TestURL
+
+by Jamal Mazrui, Consultant Access Success LLC
+
+TestURL is an open-source tool for running accessibility tests on web pages specified by URL. It generates test results in HTML format.
+
+## Installation
+
+Install Node.js and the Node Package Manager (NPM) from the [Node website](https://nodejs.org/en).
+
+Clone the repository: \
+<   git clone https://github.com/JamalMazrui/TestURL>
+
+Change to the installation directory: \
+`cd TestURL`
+
+Install support packages: \
+`npm install`
+
+## Operation
+
+Run the program: \
+`node testUrl.js <URL>`
+
+where \<URL\> is the web address of the page to be tested for conformance to the [Web Content Accessibility Guidelines](https://www.w3.org/TR/WCAG22/) (WCAG).
+
+Rather than passing a URL as a parameter, a file name may be passed instead. Such a text file lists multiple URLs, one per line. Use the syntax:
+
+`TestURL.cmd <File>`
+
+The program will run the Microsoft Edge browser with the API of [Deque Axe](https://www.npmjs.com/package/axe-core). The resulting HTML report will have a unique file name in the current directory, based on the title of the web page tested.
+
+If a URL was passed as a parameter, the HTML report will be opened when the program ends. If a file was passed instead, a report is not opened automatically.
+
+Note that automated testing only catches about a third of accessibility errors, so manual testing is also needed to evaluate WCAG conformance.
---
+++ testUrl.js
@@ -0,0 +1,81 @@
+const axeCore = require('axe-core');
+const fs = require("fs");
+const opener = require("opener");
+const path = require("path");
+const process = require("process");
+const puppeteer = require('puppeteer');
+const { reporterFactory } = require("accessibility-insights-report");
+
+function getUniqueFile(sDir, sRoot, sExt) {
+  let sPath = path.join(sDir, `${sRoot}.${sExt}`);
+  let iCount = 1;
+  while (fs.existsSync(sPath)) {
+    sPath = path.join(
+      sDir,
+      `${sRoot}-${String(iCount).padStart(2, "0")}.${sExt}`,
+    );
+    iCount++;
+  }
+  return sPath;
+}
+
+async function main(sUrl) {
+  let aLines = await [sUrl];
+let bUrl = true;
+  if (await !sUrl.includes("://")) {
+bUrl = false;
+    let sLines = await fs.readFileSync(sUrl, "utf-8");
+    aLines = await sLines.split("\n");
+  }
+
+const sCurDir = await process.cwd();
+let sOutputHtml;
+const browser = await puppeteer.launch({
+executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+headless: true
+});
+
+  while (aLines.length > 0) {
+    sUrl = await aLines.shift();
+    sUrl = await sUrl.trim();
+    if (await !sUrl) continue;
+
+console.log();
+console.log("Testing " + sUrl);
+const page = await browser.newPage();
+await page.goto(sUrl);
+
+    let sPageTitle = await page.title();
+    sPageTitle = await sPageTitle.replace(/[^a-zA-Z0-9_,#\.\-]/gi, " ");
+    sPageTitle = await sPageTitle.replace(/\s+/gi, " ").trim();
+console.log(sPageTitle);
+
+    await page.evaluate(axeCore.source);
+const results = await page.evaluate(() => {
+      return axe.run();
+    });
+
+   const reporter = reporterFactory();
+    const sHtml = reporter.fromAxeResult({
+        results,
+        description: "description of report",
+        serviceName: "service name",
+        scanContext: {
+pageTitle: sPageTitle,
+        }
+    }).asHTML();
+
+sOutputHtml = await getUniqueFile(sCurDir, sPageTitle, "htm");
+await fs.writeFileSync(sOutputHtml, sHtml);
+await page.close();
+} // while
+await browser.close();
+if (bUrl) await opener(sOutputHtml);
+}
+
+const sUrl = process.argv[2];
+if (!sUrl) {
+console.error('Please provide a URL or file as a command line argument');
+process.exit(1);
+}
+main(sUrl);
